0a6c052c823301b64858427f46f5288ab06f28c8
Nandini Nakkalakona <nandininakkalakona4@gmail.com>
2026-09-05 12:57:11 +0530

    Update program

--- program.py
+++ program.py
@@ -7,11 +7,13 @@ else:
     print("Zero")
 
 
+
 num = int(input("Enter the number"))
 if num % 2 == 0:
     print("Even")
 else:
     print("Odd")
+
 
 
 age = int(input("Enter your age: "))
@@ -22,11 +24,13 @@ else:
     print("You are not eligible to vote.")
 
 
+
 marks = int(input("Enter the marks"))
 if marks >= 35:
     print("Pass")
 else:
     print("Fail")
+
 
 
 a = 34560
@@ -35,3 +39,91 @@ if a > b:
     print("a is greater")
 else:
     print("b is greater")
+
+num=int(input("enter the number"))
+if num % 5 == 0:
+    print("Multiple of 5")
+else:
+    print("Not multiple of 5")
+
+num=int(input("enter the number"))
+if num % 3 == 0 and num % 5 == 0:
+    print("divisible by 3 and 5")
+else:
+    print("not divisible by 3 and 5")
+
+a = input("Enter a character: ")
+
+if a in "aeiouAEIOU":
+    print("Vowel")
+else:
+    print("Consonant")
+
+
+
+name = input("Enter your name: ")
+
+if name == "admin":
+    print("Welcome, Admin!")
+
+num=int(input("enter the number"))
+if num==0:
+    print("zero")
+else:
+    print("not zero")
+
+
+
+# elif statements
+a= int (input("enter the first number"))
+b= int(input("enter the second number"))
+c=int(input("enter the third number"))
+
+if a>=b and a>=c:
+    print("a is greatest")
+elif b>=a and b>=c:
+    print("b is greatest")
+else:
+    print("c is greatest")
+
+
+  
+
+num= int (input("enter the number"))
+if num>30:
+    print("its hot")
+elif num>=15 and num<=30:
+    print("pleasant")
+else:
+    print("cold")
+
+num=int(input("enter the number"))
+if num>=90:
+    print("A")
+elif num>=80 and num<=89:
+    print("B")
+elif num>=70 and num<=79:
+    print("c")
+elif num>=60 and num<=69:
+    print("d")
+else:
+    print("f")
+
+num=int(input("enter the number"))
+if num==3:
+    print("triangle")
+elif num==4:
+    print("quadrilateral")
+elif num==5:
+    print("pentagon")
+else:
+    print("unknown shape")
+     
+     
+
+
+
+
+
+
+
