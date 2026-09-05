21075c66b23a92e7b4e9e012a8eb99f5300c9c41
Nandini Nakkalakona <nandininakkalakona4@gmail.com>
2026-09-05 11:44:13 +0530

    Update Python practice programs

---
+++ program.py
@@ -0,0 +1,37 @@
+a = int(input("Enter a number: "))
+if a > 0:
+    print("Positive")
+elif a < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+
+num = int(input("Enter the number"))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+
+marks = int(input("Enter the marks"))
+if marks >= 35:
+    print("Pass")
+else:
+    print("Fail")
+
+
+a = 34560
+b = 70
+if a > b:
+    print("a is greater")
+else:
+    print("b is greater")
